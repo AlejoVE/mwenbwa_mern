d4b2aaf8216565c913993e687346d2fe0eaac983
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-03 10:09:37 +0200

    init

---
+++ src/client/app.js
@@ -0,0 +1,14 @@
+/* becodeorg/mwenbwa
+ *
+ * /src/client/app.js - Client entry point
+ *
+ * coded by leny@BeCode
+ * started at 18/05/2020
+ */
+
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+import HelloWorld from "./components/hello";
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
---
+++ src/client/components/hello.js
@@ -0,0 +1,19 @@
+/* becodeorg/mwenbwa
+ *
+ * /src/client/components/hello.js - Hello Component
+ *
+ * coded by leny@BeCode
+ * started at 18/05/2020
+ */
+
+import * as React from "react";
+
+const HelloWorld = () => (
+    <div>
+        <h1>{"Hello, Quentin!"}</h1>
+        <hr />
+        <small>{"becode/mwenbwa"}</small>
+    </div>
+);
+
+export default HelloWorld;
