40dc5d617f5804ac88b1584be7b521aa2c19673d
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-26 11:54:04 +0200

    update package json

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ import {sendLeaves, removeLeaves} from './helpers/handleLeaves'
 // const cors = require('cors')
 require('dotenv').config()
 
-const PORT = process.env.APP_PORT
+const PORT = process.env.APP_PORT || 80
 const app = express();
 
 dbConnection()
