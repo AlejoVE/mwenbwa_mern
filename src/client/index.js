32b9e2a81d3c4d29f0a7fb24b61f42dfa38a1325
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-26 15:50:21 +0200

    debug dotenv

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import "./index.css";
 
+const result = dotenv.config()
+
+if (result.error) {
+  throw result.error
+}
+
+console.log(result.parsed)
 
 ReactDOM.render(<App />, document.querySelector("#app"));
 
