f378b3334125be97b497a197eaa5a241b696a92f
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-26 12:16:06 +0200

    change port

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ import {sendLeaves, removeLeaves} from './helpers/handleLeaves'
 // const cors = require('cors')
 require('dotenv').config()
 
-const PORT = process.env.APP_PORT || 12345
+const PORT = process.env.APP_PORT || 8080
 const app = express();
 
 dbConnection()
