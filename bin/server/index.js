a3889601beb13fbbe1189c9137bad634b8d2918e
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-26 14:23:14 +0200

    add Procfile

--- bin/server/index.js
+++ bin/server/index.js
@@ -12,13 +12,11 @@ var _handleLeaves = require("./helpers/handleLeaves");
 
 require('dotenv').config();
 
-const PORT = process.env.APP_PORT;
+const PORT = process.env.APP_PORT || 3000;
 const app = (0, _express.default)();
 (0, _config.dbConnection)();
 app.use(_express.default.static(_path.default.resolve(__dirname, "../../bin/client")));
 app.use(_express.default.json());
-setInterval(_handleLeaves.sendLeaves, 1000 * 60 * 15);
-setInterval(_handleLeaves.removeLeaves, 1000 * 60 * 60);
 app.use("/api/users", require("./routes/UsersRoutes"));
 app.use("/api/trees", require("./routes/TreesRoutes"));
 app.get('/*', function (req, res) {
@@ -29,4 +27,6 @@ app.get('/*', function (req, res) {
   });
 });
 app.listen(PORT, () => console.log(`🚀 Server is listening on port ${PORT}.`));
+setInterval(_handleLeaves.sendLeaves, 1000 * 60 * 15);
+setInterval(_handleLeaves.removeLeaves, 1000 * 60 * 60);
 //# sourceMappingURL=index.js.map
