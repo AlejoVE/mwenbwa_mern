ce0690ae7a61c3246f7d4521969547f0c0a88119
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-26 15:22:03 +0200

    require .env

--- src/client/app.js
+++ src/client/app.js
@@ -2,7 +2,6 @@ import React from "react";
 import {AppRouter} from './router/appRouter';
 import {Provider} from 'react-redux';
 import {store} from './store/store';
-require('dotenv').config()
 
 
 
--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import "./index.css";
+require('dotenv').config()
+
 
 ReactDOM.render(<App />, document.querySelector("#app"));
 
