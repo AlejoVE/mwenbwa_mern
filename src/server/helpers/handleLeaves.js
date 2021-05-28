31335a278ffb36a8e28f1b8af1458ae6ff210d84
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-05-29 00:08:23 +0200

    nerf leaves every 15 minutes

--- src/server/helpers/handleLeaves.js
+++ src/server/helpers/handleLeaves.js
@@ -1,44 +1,55 @@
 const UserModel = require('../models/UserModel')
-const TreeModel = require('../models/TreeSchema')
 
 const sendLeaves = async () => {
 
     try {
-        const users = await UserModel.find().select('trees leaves')
-
+        const users = await UserModel.find().select('treesCount leaves')
         for(const user of users){
-            const trees = user.trees
-            let totalValue = 0
 
-            for(const treeId of trees){
-                const tree = await TreeModel.find({_id: treeId})
-                const value = tree[0].value
-                totalValue += value
-            }
-
-            await UserModel.findOneAndUpdate({_id: user._id}, {leaves: Math.round(user.leaves + totalValue)})
+            await UserModel.findOneAndUpdate({_id: user._id}, {leaves: Math.round(user.leaves + user.treesCount)})
         }
     } catch (err) {
         console.log(err)
     }
 }
-
+    
 const removeLeaves = async () => {
-
+    
     try {
         const users = await UserModel.find()
-
+        
         for(const user of users){
             await UserModel.findOneAndUpdate({_id: user._id}, {leaves: Math.round(user.leaves / 2)})
         }
-
+        
     } catch (err) {
         console.log(err)
     }
 }
-
-
+    
+    
 module.exports = {
     sendLeaves,
     removeLeaves
 }
+    // const sendLeaves = async () => {
+    
+    //     try {
+    //         const users = await UserModel.find().select('trees leaves')
+    
+    //         for(const user of users){
+    //             const trees = user.trees
+    //             let totalValue = 0
+    
+    //             for(const treeId of trees){
+    //                 const tree = await TreeModel.find({_id: treeId})
+    //                 const value = tree[0].value
+    //                 totalValue += value
+    //             }
+    
+    //             await UserModel.findOneAndUpdate({_id: user._id}, {leaves: Math.round(user.leaves + totalValue)})
+    //         }
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // }
